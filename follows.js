758c2065dbce52c9491b6b78030773807b2a4559
dicer999 <fraser.murray@outlook.com>
2016-05-19 15:47:04 +0100

    Update follows.js
    fixed apostrophe

--- follows.js
+++ follows.js
@@ -14,7 +14,7 @@
 
 	var defaultKey		= 'WGLAEm5JLRTRli1Wi2JuBCM18K4BhKFbx9I1ztFlTXD0JvMp', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [877880164], // Comma separated array of Xively Feed ID numbers
-		applicationName	= 'Fraser's Arduino, // Replaces Xively logo in the header
+		applicationName	= 'Test Arduino', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
